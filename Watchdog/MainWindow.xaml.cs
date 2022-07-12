456e379857bc80acb037bf68a9c72177efe43973
YungLin <raven@cathaysec.com.tw>
2022-07-12 13:30:07 +0800

    [Modify]:新增縮小到工具列

--- Watchdog/MainWindow.xaml.cs
+++ Watchdog/MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
 
 namespace Watchdog
 {
@@ -22,7 +12,10 @@ namespace Watchdog
     public partial class MainWindow : Window
     {
         #region Declarations
-        
+        private NotifyIcon notifyIcon = new NotifyIcon();
+        private ContextMenu Cms = new ContextMenu();
+        private MenuItem Mit = new MenuItem();
+        private List<MenuItem> Mits = new List<MenuItem>();
         private readonly MainWindowViewModel _main;
         #endregion
 
@@ -30,15 +23,53 @@ namespace Watchdog
         public MainWindow()
         {
             InitializeComponent();
+            setMenuItem();
+            setNotifyIcon();
             _main = new MainWindowViewModel();
             this.DataContext = _main;
         }
         #endregion
 
-        private void Window_Closed(object sender, EventArgs e)
+        private void setMenuItem()
+        {
+            Mit.Click += exit;
+            Mit.Text = "EXIT";
+            Mits.Add(Mit);
+            Cms.MenuItems.AddRange(Mits.ToArray());
+        }
+
+        private void setNotifyIcon()
+        {
+            notifyIcon.ContextMenu = Cms;
+            notifyIcon.Text = "喝水拉!";
+            notifyIcon.Icon = new Icon("drink.ico", 40, 40);
+            notifyIcon.Click += notifyIcon_Click;
+            notifyIcon.Visible = true;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;            
+            Hide();
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            if (IsVisible)
+            {
+                Activate();
+            }
+            else
+            {
+                Show();
+            }
+        }
+
+        private void exit(object sender, EventArgs e)
         {
             _main.KeepRunning = false;
             System.Environment.Exit(0);
         }
+
     }
 }
